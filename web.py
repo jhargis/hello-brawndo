d68f957c0281031e980899b4fdbd39c3dbe79e5b
Jay Hargis <jhargis@users.noreply.github.com>
2019-01-14 14:38:20 -0800

    Update web.py

--- web.py
+++ web.py
@@ -6,4 +6,5 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   PIPELINE_LOCATION = os.environ.get('PIPELINE_LOCATION', '')
-  return 'hello from %s...10' % PIPELINE_LOCATION
+  TESTVAR = os.environ.get('TESTVAR', '')
+  return 'hello from %s...10 .... TESTVAR=%s' % (PIPELINE_LOCATION, TESTVAR)
